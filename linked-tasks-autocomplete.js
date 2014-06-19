f57d9ed300ef4eb9a589e51bf5119a3261a89675
Anton Nizhegorodov <anizh3gorodov@gmail.com>
2014-06-19 12:05:59 +0300

    Parser hierarchy adjusted

--- linked-tasks-autocomplete.js
+++ linked-tasks-autocomplete.js
@@ -8,7 +8,7 @@
 
 
 $(document).ready(function() {
-    function Branch(key, parser, storage, container) {
+    function Item(key, parser, storage, container) {
         this.key     = key;
         this.parser  = parser;
         this.storage = storage;
@@ -20,8 +20,8 @@ $(document).ready(function() {
         this.push(container);
     }
         
-    Branch.prototype = {
-        constructor: Branch,
+    Item.prototype = {
+        constructor: Item,
         remove: function() {
             this.storage.removeItem(this.key);
         },
@@ -34,7 +34,7 @@ $(document).ready(function() {
                 this.remove();
                 return false;
             }
-            var data = this.el.text();
+            var data = el.text();
             if (data) {
                 this.storage.setItem(this.key, data);
             } else {
@@ -50,7 +50,6 @@ $(document).ready(function() {
             }
             var data = this.fetch() || false;
             if (data && $(container).length > 0) {
-                console.log(1);
                 $(container).val(data);
             }
         },
@@ -69,6 +68,9 @@ $(document).ready(function() {
         noElements: function() {
             return ($(this.query).length == 0) ? true : false;
         },
+        prepData: function(el) {
+            return el;
+        },
         find: function() {
             if (this.noElements()) {
                 return false;
@@ -77,9 +79,18 @@ $(document).ready(function() {
             if (results.length < this.elIndex) {
                 return false;
             }
-            var el = results.eq(this.elIndex).next();
+            var el = this.prepData(results.eq(this.elIndex));
             return (el.length == 0 || el.text() == '') ? false : el;
         }
+    };
+
+    function BranchParser(query, index) {
+        Parser.call(this, query, index);
+    }
+
+    BranchParser.prototype = new Parser();
+    BranchParser.prototype.prepData = function(el) {
+        return el.next();
     };
     
     function SubjectParser(query, index) {
@@ -87,27 +98,32 @@ $(document).ready(function() {
     }
     
     SubjectParser.prototype = new Parser();
-    SubjectParser.prototype.find = function() {
-        if (this.noElements()) {
-            return false;
-        }
-        var results = $(this.query);
-        if (results.length < this.elIndex) {
-            return false;
-        }
-        var el = results.eq(this.elIndex);
-        return (el.length == 0 || el.text() == '') ? false : el;
+
+    function DiffParser(query, index) {
+        Parser.call(this, query, index);
+    }
+
+    DiffParser.prototype = new Parser();
+    DiffParser.prototype.prepData = function(el) {
+        return el.next().children('a').eq(0);
     };
     
     var source_branches_query = 'table.attributes th:contains(Branch)',
         source_subject_query  = 'div.subject h3',
-        br_parser             = new Parser(source_branches_query, 0),
-        br_dm_parser          = new Parser(source_branches_query, 1),
-        br_cr_parser          = new Parser(source_branches_query, 2),
+        source_diffs_query    = 'table.attributes th:contains(Diff)',
+        br_parser             = new BranchParser(source_branches_query, 0),
+        br_dm_parser          = new BranchParser(source_branches_query, 1),
+        br_cr_parser          = new BranchParser(source_branches_query, 2),
         subject_parser        = new SubjectParser(source_subject_query, 0),
-        br                    = new Branch('redmine_branch', br_parser, localStorage, 'input.issue_custom_field_values_branch'),
-        br_dm                 = new Branch('redmine_dm_branch', br_dm_parser, localStorage, 'input.issue_custom_field_values_datamodel_branch'),
-        br_cr                 = new Branch('redmine_cr_branch', br_cr_parser, localStorage, 'input.issue_custom_field_values_crontab_branch'),
-    	subject               = new Branch('redmine_subject', subject_parser, localStorage, 'input#issue_subject');
+        diff_parser           = new DiffParser(source_diffs_query, 0),
+        diff_dm_parser        = new DiffParser(source_diffs_query, 1),
+        diff_cr_parser        = new DiffParser(source_diffs_query, 2),
+        br                    = new Item('redmine_branch', br_parser, localStorage, 'input.issue_custom_field_values_branch'),
+        br_dm                 = new Item('redmine_dm_branch', br_dm_parser, localStorage, 'input.issue_custom_field_values_datamodel_branch'),
+        br_cr                 = new Item('redmine_cr_branch', br_cr_parser, localStorage, 'input.issue_custom_field_values_crontab_branch'),
+    	subject               = new Item('redmine_subject', subject_parser, localStorage, 'input#issue_subject');
+    	diff                  = new Item('redmine_diff', diff_parser, localStorage, 'input.issue_custom_field_values_diff');
+    	diff_dm               = new Item('redmine_dm_diff', diff_dm_parser, localStorage, 'input.issue_custom_field_values_diff_datamodel');
+    	diff_cr               = new Item('redmine_cr_diff', diff_cr_parser, localStorage, 'input.issue_custom_field_values_diff_crontab');
 });
 
